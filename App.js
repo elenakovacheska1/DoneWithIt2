6cd06f7cb4ab4c8e7ca66a4a8d87c093e5d6ae00
Elena Kovacheska <kovacheskaelena@gmail.com>
2023-06-11 21:52:13 +0200

    Fixed padding top of main container app js

--- App.js
+++ App.js
@@ -1,31 +1,21 @@
 import Constants from "expo-constants";
 import { StyleSheet, View, Platform, StatusBar } from "react-native";
-
 import CardsScreen from "./src/screens/CardsScreen.jsx";
 
 export default function App() {
 	return (
-		<View>
+		<View style={styles.container}>
 			<StatusBar />
 			<CardsScreen />
 		</View>
 	);
 }
 
-const customStyles = StyleSheet.create({
+const styles = StyleSheet.create({
 	container: {
 		paddingTop:
 			Platform.OS === "android"
 				? StatusBar.currentHeight
 				: Constants.statusBarHeight,
-	},
-});
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
 	},
 });
